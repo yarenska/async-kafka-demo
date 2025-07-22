08cc61cbfac506391fcd0eb334969c57dc2a6e3c
yarenska <silagunes97@gmail.com>
2025-07-22 22:23:17 +0300

    test

---
+++ src/test/java/com/example/async_kafka_demo/AsyncKafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.async_kafka_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncKafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
